0efa06aca182474ba90da5afc041753496036adc
Denis Bittante <denisbittante@gmail.com>
2017-12-14 03:39:32 +0100

    styles anpassung

--- src/app/activity/activty-list/activty-list.component.ts
+++ src/app/activity/activty-list/activty-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Activity} from "../activity-model";
 
 @Component({
   selector: 'app-activty-list',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivtyListComponent implements OnInit {
 
-  constructor() { }
+  public activitylist: Activity[];
+
+  constructor() {
+
+
+  }
 
   ngOnInit() {
+    activty:Activity = new Activity();
+    this.activitylist.push(activity);
+
   }
 
 }
