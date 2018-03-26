8c575e0199b925ff962da3b4dbdcf4256948b827
denisbittante <denisbittante@gmail.com>
2018-03-26 14:08:09 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -72,7 +72,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes,{ useHash: true })),
+    RouterModule.forRoot(appRoutes,{ useHash: true }),
     ReactiveFormsModule,
     HttpClientModule,
     MatButtonModule,
