61f87222f8c866dbf1f08687edf6b7912d1eaef0
denisbittante <denisbittante@gmail.com>
2018-04-03 12:33:46 +0200

    Create activity-status-model.ts

---
+++ src/app/activity/activity-status-model.ts
@@ -0,0 +1,13 @@
+
+
+export class ActivityStatus {
+
+public statusid:number;
+public statustext:string;
+
+constructor(statusid:number,statustext:string){
+this.statusid = statusid;
+this.statustext = statustext;
+}
+
+}
